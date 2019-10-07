ef1e89128b6e204a9211c08db06fd5da12fc3790
Andrew J <53879641+andrew-jolivette@users.noreply.github.com>
2019-10-06 17:49:46 -0700

    upated survey with the ability to read the form and submit it to the app/friends route, apiRoutes updated in preparation to recieve data from survey submission

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,8 @@ module.exports = (app) => {
   app.get('/api/friends', (req, res) => {
     res.json(friends);
   })
+
+  app.post('/app/friends', (req, res) => {
+    console.log(req.body);
+  })
 };
