210772dc859c24f2a09e5bd24e2d6598808538a7
Andrew J <53879641+andrew-jolivette@users.noreply.github.com>
2019-10-06 21:31:32 -0700

    fixed bug with form submission

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,29 @@ module.exports = (app) => {
     res.json(friends);
   })
 
-  app.post('/app/friends', (req, res) => {
+  app.post('/api/friends', (req, res) => {
     console.log(req.body);
+    
+    let bestMatch = {
+      name: "",
+      picture: "",
+      friendScore: 1000
+    }
+
+    const userData = req.body;
+    const userScore = userData.scores;
+
+    var scoreDifference = 0;
+
+    for (let i = 0; i < friends.length; i++) {
+      scoreDifference = 0;
+      console.log(`friend ${i}`)
+      // for (let y = 0; y < friends[i].scores.length; y++){
+      //   console.log(`friend score ${y}`)
+      //   scoreDifference += Math.abs(parseInt(friends[i].score[y]) - parseInt(userScore[y]));
+      // }
+      console.log(scoreDifference)
+    }
+    res.json(bestMatch);  
   })
 };
